d58d40e38e93bfa8608ea8f85b49e861d9f08c55
Danny Gruebl <info@bluejade.de>
2015-04-10 12:20:14 +0200

    World and cube color rendered with colored light.

--- dev/solar-altitude.js
+++ dev/solar-altitude.js
@@ -27,44 +27,14 @@ function SolarAltitude(win, doc, $) {
     };
 
     priv.solarColor = {
-        sunrise:  { r: 100, g: 0,   b: 0   },
-        midday:   { r: 100, g: 100, b: 0   },
-        sunset:   { r: 100, g: 0,   b: 0   },
+        sunrise:  { r: 255, g: 0,   b: 0   },
+        midday:   { r: 255, g: 255, b: 0   },
+        sunset:   { r: 255, g: 0,   b: 0   },
         moonrise: { r: 0,   g: 0,   b: 50  },
         midnight: { r: 0,   g: 0,   b: 100 },
         moonset:  { r: 0,   g: 0,   b: 50  }
     };
 
-
-    priv.getBrightness = function getBrightness(time) {
-        var brightness = 0;
-
-        if (time >= 0 && time < 12) {
-            brightness = ((100 / 12) * time);
-        }
-        else {
-            brightness = 100 - ((100 / 12) * (time-12));
-        }
-
-        return brightness;
-    };
-
-    // ------------------------------------------------------------------------------------------------ Public
-
-    pub.init = function () {
-        pub.updateColors();
-
-        // Simulate a day
-        /** /
-        setInterval(function() {
-            priv.time += 0.1;
-            if (priv.time >= 24) {
-                priv.time = 0;
-            }
-            pub.updateColors();
-        }, 100);
-        /**/
-    };
 
     // Returns percentage brightness
     priv.getBrightness = function getBrightness(time) {
@@ -82,44 +52,71 @@ function SolarAltitude(win, doc, $) {
         return brightness;
     };
 
+    priv.getSolarColor = function getSolarColor(time) {
+        var solarColor = { r: 255, g: 255, b: 255 };
+
+
+
+
+        return solarColor;
+    };
+
+    // ------------------------------------------------------------------------------------------------ Public
+
+    pub.init = function () {
+        pub.updateColors();
+
+        // Simulate a day
+        /** /
+        setInterval(function() {
+            priv.time += 1;
+            if (priv.time >= 24) {
+                priv.time = 0;
+            }
+            pub.updateColors();
+        }, 100);
+        /**/
+    };
+
     pub.updateColors = function updateColors() {
         var brightness = priv.getBrightness(priv.time),
+            solarColor = priv.getSolarColor(priv.time),
             r, g, b;
 
         // Render world color
-        r = Math.round((priv.worldColor.r / 100) * brightness);
-        g = Math.round((priv.worldColor.g / 100) * brightness);
-        b = Math.round((priv.worldColor.b / 100) * brightness);
+        r = Math.round((priv.worldColor.r + solarColor.r) / 200 * brightness);
+        g = Math.round((priv.worldColor.g + solarColor.g) / 200 * brightness);
+        b = Math.round((priv.worldColor.b + solarColor.b) / 200 * brightness);
         priv.shapes.world.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
 
-
         // Render north color
-        r = Math.round((priv.cubeColor.north.r / 100) * brightness);
-        g = Math.round((priv.cubeColor.north.g / 100) * brightness);
-        b = Math.round((priv.cubeColor.north.b / 100) * brightness);
+        r = Math.round((priv.cubeColor.north.r + solarColor.r) / 200 * brightness);
+        g = Math.round((priv.cubeColor.north.g + solarColor.g) / 200 * brightness);
+        b = Math.round((priv.cubeColor.north.b + solarColor.b) / 200 * brightness);
         priv.shapes.north.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
 
         // Render north color
-        r = Math.round((priv.cubeColor.east.r / 100) * brightness);
-        g = Math.round((priv.cubeColor.east.g / 100) * brightness);
-        b = Math.round((priv.cubeColor.east.b / 100) * brightness);
+        r = Math.round((priv.cubeColor.east.r + solarColor.r) / 200 * brightness);
+        g = Math.round((priv.cubeColor.east.g + solarColor.g) / 200 * brightness);
+        b = Math.round((priv.cubeColor.east.b + solarColor.b) / 200 * brightness);
         priv.shapes.east.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
 
         // Render north color
-        r = Math.round((priv.cubeColor.south.r / 100) * brightness);
-        g = Math.round((priv.cubeColor.south.g / 100) * brightness);
-        b = Math.round((priv.cubeColor.south.b / 100) * brightness);
+        r = Math.round((priv.cubeColor.south.r + solarColor.r) / 200 * brightness);
+        g = Math.round((priv.cubeColor.south.g + solarColor.g) / 200 * brightness);
+        b = Math.round((priv.cubeColor.south.b + solarColor.b) / 200 * brightness);
         priv.shapes.south.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
 
         // Render north color
-        r = Math.round((priv.cubeColor.west.r / 100) * brightness);
-        g = Math.round((priv.cubeColor.west.g / 100) * brightness);
-        b = Math.round((priv.cubeColor.west.b / 100) * brightness);
+        r = Math.round((priv.cubeColor.west.r + solarColor.r) / 200 * brightness);
+        g = Math.round((priv.cubeColor.west.g + solarColor.g) / 200 * brightness);
+        b = Math.round((priv.cubeColor.west.b + solarColor.b) / 200 * brightness);
         priv.shapes.west.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
 
         // Logging
         /** /
         console.log(brightness);
+        console.log(solarColor);
         console.log(r);
         console.log(g);
         console.log(b);
