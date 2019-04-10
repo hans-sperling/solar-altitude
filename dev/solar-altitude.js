90b08a0bde769891ff6e0642088761f6fed83c67
GrueblD <danny.gruebl@verivox.com>
2019-04-10 15:37:46 +0200

    Change work place.

--- dev/solar-altitude.js
+++ dev/solar-altitude.js
@@ -4,12 +4,12 @@ function SolarAltitude(win, doc, $, domId, cubeColors) {
     // ----------------------------------------------------------------------------------------------- Private
 
     priv.shapes = {
-        world: $(domId),
-        north: $(domId + ' .north'),
-        east:  $(domId + ' .east'),
-        south: $(domId + ' .south'),
-        west:  $(domId + ' .west'),
-        roof:  $(domId + ' .roof')
+        world : $(domId),
+        north : $(domId + ' .north'),
+        east  : $(domId + ' .east'),
+        south : $(domId + ' .south'),
+        west  : $(domId + ' .west'),
+        front : $(domId + ' .front')
     };
 
     priv.cubeColors = cubeColors;
@@ -42,7 +42,7 @@ function SolarAltitude(win, doc, $, domId, cubeColors) {
 
     priv.solarIntensity = {
         list: [
-            // [ north, east, south, west, roof]
+            // [ north, east, south, west, front]
             [0.8, 0.5, 0.2, 0.5, 1.0], // 00:00 - Solar light shines from the north
             [0.5, 0.8, 0.5, 0.2, 1.0], // 06:00 - Solar light shines from the east
             [0.2, 0.5, 0.8, 0.5, 1.0], // 12:00 - Solar light shines from the south
@@ -88,6 +88,41 @@ function SolarAltitude(win, doc, $, domId, cubeColors) {
             index  = Math.floor(time / scale),
             result = [];
 
+        console.log('list', list);
+        if (amount == 1) {
+            result[0] = list[0];
+            result[1] = list[0];
+        }
+        else {
+            if (index < amount) {
+                result[0] = list[index];
+            }
+            else {
+                result[0] = list[0];
+            }
+
+            if ((index + 1) < amount) {
+                result[1] = list[(index + 1)];
+            }
+            else {
+                result[1] = list[0];
+            }
+        }
+
+        console.log('result', result);
+        return result;
+    };
+
+
+    priv.getListEntriesObj = function getListEntriesObj(colorObj, time) {
+        var list   = currentList.list,
+            amount = currentList.amount,
+            day    = priv.day,
+            scale  = day / amount,
+            index  = Math.floor(time / scale),
+            result = [];
+
+        console.log('list', list);
         if (amount == 1) {
             result[0] = list[0];
             result[1] = list[0];
@@ -164,20 +199,20 @@ function SolarAltitude(win, doc, $, domId, cubeColors) {
 
 
     pub.getSolarColor = function getSolarColor(time) {
-        return priv.getColor(priv.solarColors.list, time);
+        return priv.getColor(priv.solarColors, time);
     };
 
 
 
     pub.getMapColor = function getMapColor(time) {
-        return priv.getColor(priv.worldColors.list, time)
+        return priv.getColor(priv.worldColors, time)
     };
 
 
 
-    pub.getCubeColor = function getCubeColor(time) {
+    pub.getCubeColor = function getCubeColor(time, cubeColor) {
         var colorAmount = priv.cubeColors.amount,
-            colors      = priv.getListEntries(priv.cubeColors, time),
+            colors      = priv.getListEntriesObj(cubeColor, time),
             day         = priv.day,
             scale       = day / colorAmount,
             colorIndex  = Math.floor(time / scale),
@@ -214,27 +249,48 @@ function SolarAltitude(win, doc, $, domId, cubeColors) {
 
 
     pub.updateColors = function updateColors() {
+/*
         var brightness     = 100,
             time           = priv.time,
             solarColor     = { r: 255, g: 255, b: 255 },
-            solarIntensity = { north: 0, east: 0, south: 0, west: 0, roof: 0},
+            solarIntensity = { north: 0, east: 0, south: 0, west: 0, front: 0},
             worldColor     = { r: 255, g: 255, b: 255},
             cubeColor      = {
                 north: { r: 127, g: 127, b: 127 },
                 east:  { r: 127, g: 127, b: 127 },
                 south: { r: 127, g: 127, b: 127 },
                 west:  { r: 127, g: 127, b: 127 },
-                roof:  { r: 127, g: 127, b: 127 }
+                front: { r: 127, g: 127, b: 127 }
+            },
+            r, g, b;*/
+        var brightness     = 100,
+            solarColor     = { r: 255, g: 255, b: 255 },
+            solarIntensity = { north: 0, east: 0, south: 0, west: 0, front: 0},
+            worldColor     = { r: 255, g: 255, b: 255},
+            cubeColor      = {
+                north: { r: 127, g: 127, b: 127 },
+                east:  { r: 127, g: 127, b: 127 },
+                south: { r: 127, g: 127, b: 127 },
+                west:  { r: 127, g: 127, b: 127 },
+                front: { r: 127, g: 127, b: 127 }
             },
             r, g, b;
 
         // Dynamic light and brightness
         /**/
-        brightness     = pub.getBrightness(time);
-        solarColor     = pub.getSolarColor(time);
-        solarIntensity = pub.getSolarIntensity(time);
-        worldColor     = pub.getMapColor(time);
-        cubeColor      = pub.getCubeColor(time);
+        var time = priv.time;
+        var brightness     = pub.getBrightness(time);
+        var solarColor     = pub.getSolarColor(time);
+        var solarIntensity = pub.getSolarIntensity(time);
+        var worldColor     = pub.getMapColor(time);
+        var cubeColor = {
+            north: pub.getCubeColor(time, priv.cubeColors.north),
+            east:  pub.getCubeColor(time, priv.cubeColors.east),
+            south: pub.getCubeColor(time, priv.cubeColors.south),
+            west:  pub.getCubeColor(time, priv.cubeColors.west),
+            front: pub.getCubeColor(time, priv.cubeColors.front)
+        },
+
         /**/
 
         // Render world color
@@ -267,11 +323,11 @@ function SolarAltitude(win, doc, $, domId, cubeColors) {
         b = Math.round(((cubeColor[3].b + (solarColor.b * solarIntensity[3])) / (2 * 100)) * brightness);
         priv.shapes.west.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
 
-        // Render roof color
+        // Render front color
         r = Math.round(((cubeColor[4].r + solarColor.r * solarIntensity[4]) / (2 * 100)) * brightness);
         g = Math.round(((cubeColor[4].g + solarColor.g * solarIntensity[4]) / (2 * 100)) * brightness);
         b = Math.round(((cubeColor[4].b + solarColor.b * solarIntensity[4]) / (2 * 100)) * brightness);
-        priv.shapes.roof.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
+        priv.shapes.front.css('background', 'rgb(' + r + ',' + g + ',' + b + ')');
     };
 
 
@@ -292,15 +348,14 @@ jQuery(document).ready(function() {
     'use strict';
 
     var red = {
-            list: [[
-                {r: 255, g: 0, b: 0},
-                {r: 255, g: 0, b: 0},
-                {r: 255, g: 0, b: 0},
-                {r: 255, g: 0, b: 0},
-                {r: 255, g: 0, b: 0}
-            ]],
-            amount: 1
-        },
+            north : {r: 255, g: 0, b: 0},
+            east  : {r: 255, g: 0, b: 0},
+            south : {r: 255, g: 0, b: 0},
+            west  : {r: 255, g: 0, b: 0},
+            front : {r: 255, g: 0, b: 0},
+            back  : {r: 255, g: 0, b: 0}
+        };
+        /*,
         green = {
             list: [[
                 {r: 0, g: 255, b: 0},
@@ -342,15 +397,15 @@ jQuery(document).ready(function() {
                 {r: 0, g: 0, b: 255}
             ]],
             amount: 2
-        },
+        },*/
 
-        solarAltitudeRed   = new SolarAltitude(window, document, jQuery, '#appRed', red),
-        solarAltitudeGreen = new SolarAltitude(window, document, jQuery, '#appGreen', green),
+        var solarAltitudeRed   = new SolarAltitude(window, document, jQuery, '#appRed', red);
+        /*solarAltitudeGreen = new SolarAltitude(window, document, jQuery, '#appGreen', green),
         solarAltitudeBlue  = new SolarAltitude(window, document, jQuery, '#appBlue', blue),
-        solarAltitudeGrey  = new SolarAltitude(window, document, jQuery, '#appGrey', grey);
+        solarAltitudeGrey  = new SolarAltitude(window, document, jQuery, '#appGrey', grey);*/
 
-    solarAltitudeRed.init();
+    solarAltitudeRed.init();/*
     solarAltitudeGreen.init();
     solarAltitudeBlue.init();
-    solarAltitudeGrey.init();
+    solarAltitudeGrey.init();*/
 });
